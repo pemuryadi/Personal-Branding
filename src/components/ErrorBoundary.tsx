956e45091b0e1414cf5c6a4e5d031d680b7c704e
pemuryadi <131847951+pemuryadi@users.noreply.github.com>
2026-06-01 14:27:22 +0800

    fix: resolve Cloudflare build crash by removing package-lock.json (sync issue) and fixing TS errors

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -10,12 +10,8 @@ interface State {
 }
 
 export class ErrorBoundary extends React.Component<Props, State> {
-  public props: Props;
-  public state: State;
-
   constructor(props: Props) {
     super(props);
-    this.props = props;
     this.state = {
       hasError: false,
       error: null,
